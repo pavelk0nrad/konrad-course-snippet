170dc7624a67825209d2a538a779156be7d9ed21
Pavel Konrad <115944509+pavelk0nrad@users.noreply.github.com>
2024-09-25 12:09:52 +0200

    Create 01_pole.js

---
+++ 45.05 JS Pole/01_pole.js
@@ -0,0 +1,18 @@
+let J = 20
+let Q = 30
+let K = 40
+let A = 50
+
+let hearts = ['♥️', 7, 8, 9, 10, J, Q, K, A]
+let diamonds = ['♦️', 7, 8, 9, 10, J, Q, K, A]
+let spades = ['♣️',7, 8, 9, 10, J, Q, K, A]
+let clubs = ['♠️',7, 8, 9, 10, J, Q, K, A]
+
+let deck = [hearts, diamonds, spades, clubs];
+
+let player = [
+];
+
+let enemy = [
+
+];
