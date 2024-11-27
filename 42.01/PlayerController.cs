881fc10e608f07b01ab1006a5428a8a283f70a07
Pavel Konrad <115944509+pavelk0nrad@users.noreply.github.com>
2024-11-27 13:50:52 +0100

    Create PlayerController.cs

---
+++ 42.01/PlayerController.cs
@@ -0,0 +1,13 @@
+private float speed = 20.0f;
+private float turnSpeed = 45.0f;
+private float horizontalInput;
+private float forwardInput;
+
+void Update() {
+  horizontalInput = Input.GetAxis("Horizontal");
+  forwardInput = Input.GetAxis("Vertical");
+  // Moves the car forward based on vertical input
+  transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+  // Rotates the car based on horizontal input
+  transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+}
